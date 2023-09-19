b1a143dd54279e15f125b3e84c53c8821e3e4e72
Leeya Davis <16376036+LeeyaD@users.noreply.github.com>
2023-09-19 11:55:17 -0400

    updated file structure day1 setup
    Co-authored-by: Chris Brum <christopher.brum@gmail.com>

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const App = () => {
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(App());
