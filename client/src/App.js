a1f30ecf86d84a54c57d4cf753d68d5ad86cb61b
Christopher Brum <christopher.brum@gmail.com>
2023-09-21 11:42:12 -0700

    Added basic cart functionality
    Co-authored-by: Leeya Davis: <leeya463@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import Form from './components/Form.js'
 import { addProduct, getProducts } from './services/product.js'
 
 const App = () => {
-
   const [formVisible, setFormVisible] = useState(false)
   const [products, setProducts] = useState([])
+  const [cartItems, setCartItems] = useState([])
 
 	useEffect(() => {
     const fetchData = async() => {
@@ -27,26 +27,77 @@ const App = () => {
     document.querySelector(".add-form").classList.toggle("visible")
   }
 
-  // onSubmit event handler, takes 3 pieces of state
-  // sends POST req to axios '/products' 
+  const handleCartUpdate = (product) => {
+    let itemFound = false;
+
+    const nextCartItems = cartItems.map(item => {
+      const newItem = {...item}
+      if (item._id === product._id) {
+        newItem.cartQuantity++
+        itemFound = true
+      }
+      return newItem
+    })
+
+    if (!itemFound) {
+      const newCartItem = {...product}
+      newCartItem.cartQuantity = 1
+      setCartItems(nextCartItems.concat(newCartItem))
+    } else {
+      setCartItems(nextCartItems)
+    }
+  }
+
+  const calcCartTotal = () => {
+    let total = 0
+    cartItems.forEach(item => {
+      total += item.cartQuantity * item.price
+    })
+    return total.toFixed(2)
+  }
 
 	return (
 		<div id="app">
       <header>
-        <h1>The Shop!</h1>
-        <div className="cart">
-          <h2>Your Cart</h2>
-          <p>Your cart is empty</p>
-          <p>Total: $0</p>
-          <button className="checkout" disabled>Checkout</button>
-        </div>
-      </header>
+      <h1>The Shop!</h1>
+      <div className="cart">
+      <h2>Your Cart</h2>
+      <table className="cart-items">
+        <thead>
+          <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map(item => {
+            return (
+              <tr key={item._id}>
+                <td>{item.title}</td>
+                <td>{item.cartQuantity}</td>
+                <td>${item.price}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="3" className="total">Total: ${calcCartTotal()}</td>
+          </tr>
+        </tfoot>
+      </table>
+      <div className="checkout-button">
+        <button className="checkout">Checkout</button>
+      </div>
+    </div>
+    </header>
 
     <main>
       <div className="product-listing">
         <h2>Products</h2>
         <ul className="product-list">
-					{products.map(product => <Product key={product["_id"]} {...product} />)}
+					{products.map(product => <Product cartItems={cartItems} onAddItem={handleCartUpdate} product={product} key={product["_id"]} />)}
         </ul>
       </div>
       <div className="add-form">
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,19 +1,40 @@
-const Product = ({ title, price, quantity }) => {
+const Product = ({ product, onAddItem, cartItems }) => {
+  const { title, price, quantity } = product
+
+  const findProduct = (productId) => {
+    for (let i = 0; i < cartItems.length; i += 1) {
+      if (cartItems[i]._id === productId) {
+        return cartItems[i]
+      }
+    }
+  }
+
+  const currentQuantity = () => {
+    const cartItem = findProduct(product._id)
+    let itemQuantityInCart;
+    if (!cartItem) {
+      itemQuantityInCart = 0 
+    } else {
+      itemQuantityInCart = cartItem.cartQuantity
+    }
+    return quantity - itemQuantityInCart
+  }
+
   return (
     <li className="product">
       <div className="product-details">
         <h3>{title}</h3>
         <p className="price">${price}</p>
-        {quantity === 0 ? 
-          <p className="quantity none-left">{quantity} left in stock</p>
+        {currentQuantity() === 0 ? 
+          <p className="quantity none-left">{currentQuantity()} left in stock</p>
         :
-          <p className="quantity">{quantity} left in stock</p>
+          <p className="quantity">{currentQuantity()} left in stock</p>
         }
         <div className="actions product-actions">
-					{quantity === 0 ?
+					{currentQuantity() === 0 ?
 						<button className="add-to-cart" disabled>Add to Cart</button>
 						: 
-						<button className="add-to-cart">Add to Cart</button>
+						<button onClick={() => onAddItem(product)} className="add-to-cart">Add to Cart</button>
 					}
           <button className="edit">Edit</button>
         </div>
